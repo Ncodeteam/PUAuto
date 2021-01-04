513a6261918a0744a74dff22d207e8dc2a84b81e
Ncode2014 <32153003+Ncode2014@users.noreply.github.com>
2021-01-04 20:18:34 +0700

    2.1
    Added NekoX
    Added .apk cleaner & changing some stuff

--- scripts/cleaner.sh
+++ scripts/cleaner.sh
@@ -24,8 +24,9 @@ find Android/data/ -depth -name "*opera*" -type d -exec rm -rf "{}" \;
 find Android/data/ -depth -name "*mi*" -type d -exec rm -rf "{}" \; 
 find Android/data/ -depth -name "*xiaomi*" -type d -exec rm -rf "{}" \; 
 
-#khusus pengguna mimusic sampah
+#music cache folder
 find . -depth -name "*joox*" -type d -exec rm -rf "{}" \; 2>/dev/null
+find . -depth -name "*spotify*" -type d -exec rm -rf "{}" \; 2>/dev/null
 
 # khusus google dan android
 find Android/data/ -depth -name "*google*" -type d -exec rm -rf "{}" \; 
@@ -44,6 +45,12 @@ find Telegram/ -type f -name "*.sh" -exec rm -f {} \;  2>/dev/null
 find Telegram/ -type f -name "*.zip" -exec rm -f {} \;  2>/dev/null
 rm -rf Android/data/org.telegram.messenger/cache 2>/dev/null
 
+#neko X 
+rm -rf Android/data/nekox.messanger/files/caches 2>/dev/null
+find Android/data/nekox.messanger/files/ -type f -name "*.apk" -exec rm -f {} \; 2>/dev/null
+find Android/data/nekox.messanger/files/ -type f -name "*.sh" -exec rm -f {} \;  2>/dev/null
+find Android/data/nekox.messanger/files/ -type f -name "*.zip" -exec rm -f {} \;  2>/dev/null
+
 # telegram x cleaner
 find Android/data/org.thunderdog.challegram/ -type f -name "*.apk" -exec rm -f {} \; 2>/dev/null
 find Android/data/org.thunderdog.challegram/ -type f -name "*.jpg" -exec rm -f {} \; 2>/dev/null
@@ -58,9 +65,13 @@ find . -depth -type d -empty -exec rmdir {} \;
 #log & more
 find . -depth -name "*log*" -type d -exec rm -rf "{}" \; 2>/dev/null
 find . -depth -name "*thumbnails*" -type d -exec rm -rf "{}" \; 
+find . -depth -name "*.log" -type f -exec rm -f "{}" \; 2>/dev/null
 
-# penghilang anu biar bkp nya kelihatan xixixiixx
+# nomedia yang berarti akan membuat semua yang gk ingin dibaca galeri akhirnya terbaca
 find . -type f -name "*nomedia*" -exec rm -f {} \; 
+
+# penghapusan apk biar gk numpuk
+find . -type f -name "*.apk" -exec rm -f {} \; 
 
 # telegram X issue 
 # i found this issue when i workaround this script
