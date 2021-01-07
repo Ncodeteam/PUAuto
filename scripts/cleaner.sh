dde147017bcc9b9ed95ee1ce3289646fb79b006d
Ncode2014 <32153003+Ncode2014@users.noreply.github.com>
2021-01-07 12:34:08 +0700

    Fix Bug minor
    Fix Genshin Impact and some app deleted forced without reason
    Removing some stuff
    Added cache cleaner

--- scripts/cleaner.sh
+++ scripts/cleaner.sh
@@ -51,6 +51,7 @@ find Whatsapp/ -depth -name "*Backups*" -type d -exec rm -rf "{}" \;
 find Whatsapp/ -depth -name "*Databases*" -type d -exec rm -rf "{}" \;
 
 echo "moar cleaning unecessery"
+
 #original telegram cleaner
 find Telegram/ -type f -name "*.apk" -exec rm -f {} \; 2>/dev/null
 find Telegram/ -type f -name "*.sh" -exec rm -f {} \;  2>/dev/null
@@ -71,9 +72,25 @@ find Android/data/org.thunderdog.challegram/ -type f -name "*.sh" -exec rm -f {}
 find Android/data/org.thunderdog.challegram/ -type f -name "*.zip" -exec rm -f {} \; 2>/dev/null
 rm -rf Android/data/org.thunderdog.challegram/cache 2>/dev/null
 
-# delete file kosong
+# genshin impact & some stuff
+if [ -d Android/data/com.miHoYo.GenshinImpact ]; then
+echo "data genshin ditemukan jangan hapus"
+else
 find Android/ -depth -type d -empty -exec rmdir {} \;
-find . -depth -type d -empty -exec rmdir {} \;
+
+if [ -d Android/data/com.iudesk.android.photo.editor ]; then
+echo "data photo editor ditemukan jangan hapus"
+else
+find Android/ -depth -type d -empty -exec rmdir {} \;
+
+if [ -d Android/data/mark.via.gp ]; then
+echo "data via ditemukan jangan dihapus"
+else
+find Android/ -depth -type d -empty -exec rmdir {} \;
+
+# delete file kosong
+#find Android/ -depth -type d -empty -exec rmdir {} \;
+#find . -depth -type d -empty -exec rmdir {} \;
 
 #log & more
 find . -depth -name "*log*" -type d -exec rm -rf "{}" \; 2>/dev/null
@@ -86,6 +103,9 @@ find . -type f -name "*nomedia*" -exec rm -f {} \;
 # penghapusan apk biar gk numpuk
 find . -type f -name "*.apk" -exec rm -f {} \; 
 
+#some function
+find Android/data -depth -name "*cache*" -type d -exec rm -rf "{}" \; 
+
 # telegram X issue 
 # i found this issue when i workaround this script
 # so i recreate again this folder to fix that
@@ -93,14 +113,29 @@ find . -type f -name "*.apk" -exec rm -f {} \;
 
 if [ -d Android/data/org.thunderdog.challegram/files ];
 then 
-    mkdir Android/data/org.thunderdog.challegram/files/animations
-    mkdir Android/data/org.thunderdog.challegram/files/documents
-    mkdir Android/data/org.thunderdog.challegram/files/music
-    mkdir Android/data/org.thunderdog.challegram/files/photos
-    mkdir Android/data/org.thunderdog.challegram/files/temp
-    mkdir Android/data/org.thunderdog.challegram/files/video_notes
-    mkdir Android/data/org.thunderdog.challegram/files/videos
-    mkdir Android/data/org.thunderdog.challegram/files/voice
+    mkdir Android/data/org.thunderdog.challegram/files/animations 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/documents 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/music 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/photos 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/temp 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/video_notes 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/videos 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/voice 2>/dev/null
+    else
+    echo 
+fi
+
+# if you have secoundary account telegram x
+if [ -d Android/data/org.thunderdog.challegram/files/x_account1 ];
+then 
+    mkdir Android/data/org.thunderdog.challegram/files/x_account1/animations 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/x_account1/documents 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/x_account1/music 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/x_account1/photos 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/x_account1/temp 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/x_account1/video_notes 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/x_account1/videos 2>/dev/null
+    mkdir Android/data/org.thunderdog.challegram/files/x_account1/voice 2>/dev/null
     else
     echo 
 fi
@@ -144,54 +179,9 @@ then
     else 
     echo 
 fi
-
-if [ -d /data ];
-then
-      rm -r /data/dalvik-cache 2>/dev/null
-      rm -rf /cache/*.apk 2>/dev/null
-      rm -f /cache/*.tmp 2>/dev/null
-      rm -f /data/*.log 2>/dev/null
-      rm -f /data/*.txt 2>/dev/null
-      rm -f /data/anr/* 2>/dev/null
-      rm -f /data/backup/pending/*.tmp 2>/dev/null
-      rm -f /data/cache/*.* 2>/dev/null
-      rm -f /data/data/*.log 2>/dev/null
-      rm -f /data/data/*.txt 2>/dev/null
-      rm -f /data/log/*.log 2>/dev/null
-      rm -f /data/log/*.txt 2>/dev/null
-      rm -f /data/local/*.apk 2>/dev/null
-      rm -f /data/local/*.log 2>/dev/null
-      rm -f /data/local/*.txt  2>/dev/null
-      rm -f /data/local/tmp/* 2>/dev/null
-      rm -f /data/last_alog/*.log 2>/dev/null
-      rm -f /data/last_alog/*.txt 2>/dev/null
-      rm -f /data/last_kmsg/*.log 2>/dev/null
-      rm -f /data/last_kmsg/*.txt 2>/dev/null
-      rm -f /data/mlog/* 2>/dev/null
-      rm -f /data/system/*.log 2>/dev/null
-      rm -f /data/system/*.txt 2>/dev/null
-      rm -f /data/system/dropbox/* 2>/dev/null
-      rm -rf /data/system/usagestats/* 2>/dev/null
-      rm -f /data/system/shared_prefs/* 2>/dev/null
-      rm -f /data/tombstones/* 2>/dev/null
-      rm -f /data/dalvik-cache/*.apk 2>/dev/null
-      rm -f /data/dalvik-cache/*.tmp 2>/dev/null
-      rm -f /data/last_alog/*.log 2>/dev/null
-      rm -f /data/last_alog/*.txt 2>/dev/null
-      rm -f /data/last_kmsg/*.log 2>/dev/null
-      rm -f /data/last_kmsg/*.txt 2>/dev/null
-      rm -f /data/mlog/* 2>/dev/null
-      rm -f /data/system/*.log 2>/dev/null
-      rm -f /data/system/*.txt 2>/dev/null
-      rm -f /data/system/dropbox/* 2>/dev/null
-      rm -rf /data/system/usagestats/* 2>/dev/null
-      rm -f /data/system/shared_prefs/* 2>/dev/null
-      rm -f /data/tombstones/* 2>/dev/null
-    else 
-    echo 
-fi
-
 # my friend has reported some annoying folder call "browsermetrics"
 # so i make it to force to delete them
 
-find /data/user -depth -name "*BrowserMetrics*" -type d -exec rm -rf "{}" \; 2>/dev/null
+find /data/user/ -depth -name "*BrowserMetrics*" -type d -exec rm -rf "{}" \; 2>/dev/null
+
+echo "pembersihan selesai"
