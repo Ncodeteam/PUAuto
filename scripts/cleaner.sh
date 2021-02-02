2666fd56742f699ba97dd0f1c67a2ee5d30976cd
Ncode2014 <32153003+Ncode2014@users.noreply.github.com>
2021-02-02 14:06:46 +0700

    Update fix
    Fix genshin impact issue (again)
    And changing logic for telegram cleaner to easier method

--- scripts/cleaner.sh
+++ scripts/cleaner.sh
@@ -25,6 +25,11 @@ rm -rf Android/data/*xiaomi*/
 rm -rf Android/data/*MusicApp*/
 
 #trash removed
+# another bug genshin so i make this to fix
+if [ -d Android/data/com.miHoYo.GenshinImpact ];
+then
+echo "skipped"
+else
 rm -rf Android/data/*mi*/
 rm -rf Android/data/*lenovo*/
 rm -rf Android/data/*snssdk*/
@@ -35,6 +40,7 @@ rm -rf Android/data/*UCMobile*/
 rm -rf Android/data/*babe*/
 rm -rf Android/data/*group*/
 rm -rf Android/data/*care*/
+fi
 
 #testing 
 rm -rf Android/data/*Music*/
@@ -52,11 +58,11 @@ rm -rf Android/data/*android*/
 
 # khusus whatsapp
 echo "hapus whatsapp useless folder"
-find Whatsapp/ -depth -name "*Shared*" -type d -exec rm -rf "{}" \; 
-find Whatsapp/ -depth -name "*Thumbs*" -type d -exec rm -rf "{}" \; 
-find Whatsapp/ -depth -name "*trash*" -type d -exec rm -rf "{}" \; 
-find Whatsapp/ -depth -name "*Backups*" -type d -exec rm -rf "{}" \; 
-find Whatsapp/ -depth -name "*Databases*" -type d -exec rm -rf "{}" \;
+find WhatsApp/ -depth -name "*Shared*" -type d -exec rm -rf "{}" \; 
+find WhatsApp/ -depth -name "*Thumbs*" -type d -exec rm -rf "{}" \; 
+find WhatsApp/ -depth -name "*trash*" -type d -exec rm -rf "{}" \; 
+find WhatsApp/ -depth -name "*Backups*" -type d -exec rm -rf "{}" \; 
+find WhatsApp/ -depth -name "*Databases*" -type d -exec rm -rf "{}" \;
 
 echo "moar cleaning unecessery"
 
@@ -89,46 +95,49 @@ find Android/data -depth -name "*cache*" -type d -exec rm -rf "{}" \;
 echo "membersihkan telegram"
 
 #original telegram cleaner
-find Telegram/ -type f -name "*.apk" -exec rm -f {} \; 2>/dev/null
-find Telegram/ -type f -name "*.apk" -exec rm -f {} \; 2>/dev/null
-find Telegram/ -type f -name "*.mp4" -exec rm -f {} \; 2>/dev/null
-find Telegram/ -type f -name "*.jpg" -exec rm -f {} \; 2>/dev/null
-find Telegram/ -type f -name "*.oga" -exec rm -f {} \; 2>/dev/null
-find Telegram/ -type f -name "*.pdf" -exec rm -f {} \; 2>/dev/null
-find Telegram/ -type f -name "*.flac" -exec rm -f {} \; 2>/dev/null
-find Telegram/ -type f -name "*.txt" -exec rm -f {} \; 2>/dev/null
-find Telegram/ -type f -name "*.png" -exec rm -f {} \; 2>/dev/null
-find Telegram/ -type f -name "*.sh" -exec rm -f {} \; 2>/dev/null
-find Telegram/ -type f -name "*.zip" -exec rm -f {} \; 2>/dev/null
-find Telegram/ -type f -name "*.mp3" -exec rm -f {} \; 2>/dev/null
+rm -f Telegram/*.apk
+rm -f Telegram/*.mp4
+rm -f Telegram/*.jpg
+rm -f Telegram/*.oga
+rm -f Telegram/*.pdf
+rm -f Telegram/*.flac
+rm -f Telegram/*.ehi
+rm -f Telegram/*.txt
+rm -f Telegram/*.png
+rm -f Telegram/*.sh
+rm -f Telegram/*.zip
+rm -f Telegram/*.mp3
 rm -rf Android/data/org.telegram.messenger/cache 2>/dev/null
 
 #neko X 
 rm -rf Android/data/nekox.messanger/files/caches 2>/dev/null
-find Android/data/nekox.messanger/files -type f -name "*.apk" -exec rm -f {} \; 2>/dev/null
-find Android/data/nekox.messanger/files -type f -name "*.mp4" -exec rm -f {} \; 2>/dev/null
-find Android/data/nekox.messanger/files -type f -name "*.jpg" -exec rm -f {} \; 2>/dev/null
-find Android/data/nekox.messanger/files -type f -name "*.oga" -exec rm -f {} \; 2>/dev/null
-find Android/data/nekox.messanger/files -type f -name "*.pdf" -exec rm -f {} \; 2>/dev/null
-find Android/data/nekox.messanger/files -type f -name "*.flac" -exec rm -f {} \; 2>/dev/null
-find Android/data/nekox.messanger/files -type f -name "*.txt" -exec rm -f {} \; 2>/dev/null
-find Android/data/nekox.messanger/files -type f -name "*.png" -exec rm -f {} \; 2>/dev/null
-find Android/data/nekox.messanger/files -type f -name "*.sh" -exec rm -f {} \; 2>/dev/null
-find Android/data/nekox.messanger/files -type f -name "*.zip" -exec rm -f {} \; 2>/dev/null
-find Android/data/nekox.messanger/files -type f -name "*.mp3" -exec rm -f {} \; 2>/dev/null
+rm -f Android/data/nekox.messanger/files/*.apk
+rm -f Android/data/nekox.messanger/files/*.mp4
+rm -f Android/data/nekox.messanger/files/*.ehi
+rm -f Android/data/nekox.messanger/files/*.jpg
+rm -f Android/data/nekox.messanger/files/*.oga
+rm -f Android/data/nekox.messanger/files/*.pdf
+rm -f Android/data/nekox.messanger/files/*.flac
+rm -f Android/data/nekox.messanger/files/*.txt
+rm -f Android/data/nekox.messanger/files/*.png
+rm -f Android/data/nekox.messanger/files/*.sh
+rm -f Android/data/nekox.messanger/files/*.zip
+rm -f Android/data/nekox.messanger/files/*.mp3
 
 # telegram x cleaner
-find Android/data/org.thunderdog.challegram/ -type f -name "*.apk" -exec rm -f {} \; 2>/dev/null
-find Android/data/org.thunderdog.challegram/ -type f -name "*.mp4" -exec rm -f {} \; 2>/dev/null
-find Android/data/org.thunderdog.challegram/ -type f -name "*.jpg" -exec rm -f {} \; 2>/dev/null
-find Android/data/org.thunderdog.challegram/ -type f -name "*.oga" -exec rm -f {} \; 2>/dev/null
-find Android/data/org.thunderdog.challegram/ -type f -name "*.pdf" -exec rm -f {} \; 2>/dev/null
-find Android/data/org.thunderdog.challegram/ -type f -name "*.flac" -exec rm -f {} \; 2>/dev/null
-find Android/data/org.thunderdog.challegram/ -type f -name "*.txt" -exec rm -f {} \; 2>/dev/null
-find Android/data/org.thunderdog.challegram/ -type f -name "*.png" -exec rm -f {} \; 2>/dev/null
-find Android/data/org.thunderdog.challegram/ -type f -name "*.sh" -exec rm -f {} \; 2>/dev/null
-find Android/data/org.thunderdog.challegram/ -type f -name "*.zip" -exec rm -f {} \; 2>/dev/null
-find Android/data/org.thunderdog.challegram/ -type f -name "*.mp3" -exec rm -f {} \; 2>/dev/null
+rm -f 
+rm -f Android/data/org.thunderdog.challegram/*.apk
+rm -f Android/data/org.thunderdog.challegram/*.mp4
+rm -f Android/data/org.thunderdog.challegram/*.ehi
+rm -f Android/data/org.thunderdog.challegram/*.jpg
+rm -f Android/data/org.thunderdog.challegram/*.oga
+rm -f Android/data/org.thunderdog.challegram/*.pdf
+rm -f Android/data/org.thunderdog.challegram/*.flac
+rm -f Android/data/org.thunderdog.challegram/*.txt
+rm -f Android/data/org.thunderdog.challegram/*.png
+rm -f Android/data/org.thunderdog.challegram/*.sh
+rm -f Android/data/org.thunderdog.challegram/*.zip
+rm -f Android/data/org.thunderdog.challegram/*.mp3
 rm -rf Android/data/org.thunderdog.challegram/cache 2>/dev/null
 
 # telegram X issue 
@@ -138,6 +147,8 @@ rm -rf Android/data/org.thunderdog.challegram/cache 2>/dev/null
 
 if [ -d Android/data/org.thunderdog.challegram/files ];
 then 
+    echo
+    else
     mkdir Android/data/org.thunderdog.challegram/files/animations 2>/dev/null
     mkdir Android/data/org.thunderdog.challegram/files/documents 2>/dev/null
     mkdir Android/data/org.thunderdog.challegram/files/music 2>/dev/null
@@ -146,13 +157,13 @@ then
     mkdir Android/data/org.thunderdog.challegram/files/video_notes 2>/dev/null
     mkdir Android/data/org.thunderdog.challegram/files/videos 2>/dev/null
     mkdir Android/data/org.thunderdog.challegram/files/voice 2>/dev/null
-    else
-    echo 
 fi
 
 # if you have secoundary account telegram x
 if [ -d Android/data/org.thunderdog.challegram/files/x_account1 ];
 then 
+    echo
+    else
     mkdir Android/data/org.thunderdog.challegram/files/x_account1/animations 2>/dev/null
     mkdir Android/data/org.thunderdog.challegram/files/x_account1/documents 2>/dev/null
     mkdir Android/data/org.thunderdog.challegram/files/x_account1/music 2>/dev/null
@@ -161,8 +172,6 @@ then
     mkdir Android/data/org.thunderdog.challegram/files/x_account1/video_notes 2>/dev/null
     mkdir Android/data/org.thunderdog.challegram/files/x_account1/videos 2>/dev/null
     mkdir Android/data/org.thunderdog.challegram/files/x_account1/voice 2>/dev/null
-    else
-    echo 
 fi
 
 # some yeah for MIUI
