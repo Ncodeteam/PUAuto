c19f8b47a1621219fc450e7794f91d2985985371
Ncode2014 <Private.nafi2016@gmail.com>
2021-04-09 10:17:13 +0700

    refactoring
    with some naise thing

--- config.sh
+++ config.sh
@@ -5,6 +5,13 @@
 # just a note i not responsible if you found this script to make something illegal
 # so don't report me as author on this script
 
+# infooooo
+PUBGKR=/storage/emulated/0/Android/data/com.pubg.krmobile
+PUBG=/storage/emulated/0/Android/data/com.tencent.ig
+DOWN=/storage/emulated/0/Download/
+NORMAL=/storage/emulated/0/
+PUBGD=Android/data/com.tencent.ig
+
 clear
 echo "==================================="
 echo -e "2.3 PUBG INSTALLER CONFIG & BOOSTER"
@@ -12,16 +19,13 @@ echo -e "created by nekaru"
 echo -e "note : jika kamu terkena banned jangan pernah salahkan installer ini"
 echo -e "resiko ditannggung sendiri"
 echo "==================================="
-sleep 5
-echo "menuju tkp"
 sleep 2
 clear
 
 # cangluk
 echo -e "========================================="
 echo -e "changelog"
-echo -e "a).Reworked Script to activate again"
-echo -e "b).Make a easier method"
+echo -e "a).Refactoring and Added new extension support"
 echo -e "========================================="
 sleep 5
 echo "pembersihan"
@@ -38,11 +42,11 @@ sleep 1
 echo -ne '=================         (60%)\r'
 sleep 1
 echo -ne '=====================     (80%)\r'
-# reworked engine bash for pubg mobile global and korean
-find /storage/emulated/0/Android/data/com.tencent.ig -type d \( -name "*TGPA*" -o -name "*ProgramBinaryCache*" -o -name "*cache*" -o -name "*Logs*" -o -name "*Pandora*" -o -name "*LightData*" -o -name "*GameErrorNoRecords*" -o -name "*StatEventReportedFlag*" -o -name "*PufferEifs0*" -o -name "*PufferEifs1*" \) -exec rm -rf "{}" \;  2>/dev/null
+# reworked engine bash for pubg mobile global and korean maybe i want to support to another pubg but i think maybe
+find $PUBG  -type d \( -name "*TGPA*" -o -name "*ProgramBinaryCache*" -o -name "*cache*" -o -name "*Logs*" -o -name "*Pandora*" -o -name "*LightData*" -o -name "*GameErrorNoRecords*" -o -name "*StatEventReportedFlag*" -o -name "*PufferEifs0*" -o -name "*PufferEifs1*" \) -exec rm -rf "{}" \;  2>/dev/null
 rm Android/data/com.tencent.ig/files/cacheFile.txt 2>/dev/null
 
-find /storage/emulated/0/Android/data/com.pubg.krmobile -type d \( -name "*TGPA*" -o -name "*ProgramBinaryCache*" -o -name "*cache*" -o -name "*Logs*" -o -name "*Pandora*" -o -name "*LightData*" -o -name "*GameErrorNoRecords*" -o -name "*StatEventReportedFlag*" -o -name "*PufferEifs0*" -o -name "*PufferEifs1*" \) -exec rm -rf "{}" \;  2>/dev/null
+find $PUBGKR -type d \( -name "*TGPA*" -o -name "*ProgramBinaryCache*" -o -name "*cache*" -o -name "*Logs*" -o -name "*Pandora*" -o -name "*LightData*" -o -name "*GameErrorNoRecords*" -o -name "*StatEventReportedFlag*" -o -name "*PufferEifs0*" -o -name "*PufferEifs1*" \) -exec rm -rf "{}" \;  2>/dev/null
 rm Android/data/com.pubg.krmobile/files/cacheFile.txt 2>/dev/null
 sleep 1
 echo -ne '======================   (100%)\r'
@@ -55,12 +59,15 @@ clear
 # reworked for pak and enjoy
 
 # installer simple for pubg since i dont want any complex INSTALLER
-if [-d Android/data/com.tencent.ig ];
+if [-d $PUBGD ];
 then
 echo "installing....."
-find /storage/emulated/0/Download/ -name '*.pak' -exec cp "{}" Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/ \;
-find /storage/emulated/0/ -name '*.cache' -exec cp "{}" Android/data/com.tencent.ig/files/ \;
-find /storage/emulated/0/ -name '*EnjoyCJZC*.ini' -exec cp "{}" Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/ \;
-find /storage/emulated/0/ -name '*GameUserSettings*.ini' -exec cp "{}" Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/ \;
+find $DOWN -name '*.pak' -exec cp "{}" Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/ \;
+find $NORMAL -name '*.cache' -exec cp "{}" Android/data/com.tencent.ig/files/ \;
+find $NORMAL -name '*EnjoyCJZC*.ini' -exec cp "{}" Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/ \;
+find $NORMAL -name '*GameUserSettings*.ini' -exec cp "{}" Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/ \;
+find $DOWN -name '*.sav' -exec cp "{}" Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/
 echo "done!"
+else
+echo "some file maybe lost but if you dont know about that ignore this message"
 fi
